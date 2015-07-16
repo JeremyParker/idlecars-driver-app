8a19f2f7fd5276e52a7534b8b0ca82f79dea6251
jefk <speedracer111@gmail.com>
2015-07-16 16:28:53 -0700

    label the filter

--- src/app/cars/list.controller.js
+++ src/app/cars/list.controller.js
@@ -6,14 +6,15 @@ angular.module('idlecars')
 
   CarService.query().$promise.then(function(cars) {
     _allCars = cars;
-    $scope.cars = cars;
+    _filterCars();
   });
 
   var costBuckets = {
-    0: {name: 'less than $50', min: 0, max: 50},
-    1: {name: '$50 to $75', min: 50, max: 75},
-    2: {name: '$75 to $100', min: 75, max: 100},
-    3: {name: '$100 or more', min: 100, max: 9999},
+    0: {name: 'any price', min: 0, max: 9999},
+    1: {name: 'less than $50', min: 0, max: 50},
+    2: {name: '$50 to $75', min: 50, max: 75},
+    3: {name: '$75 to $100', min: 75, max: 100},
+    4: {name: '$100 or more', min: 100, max: 9999},
   }
 
   $scope.$on("slideEnded", function() {
@@ -21,11 +22,9 @@ angular.module('idlecars')
   });
 
   var _filterCars = function() {
-    $scope.cars = attrInRangeFilter(
-      _allCars,
-      'cost',
-      costBuckets[$scope.costBucket].min,
-      costBuckets[$scope.costBucket].max
-    );
+    var bucket = costBuckets[$scope.costBucket];
+
+    $scope.costFilterLabel = bucket.name;
+    $scope.cars = attrInRangeFilter(_allCars, 'cost', bucket.min, bucket.max);
   }
 })
