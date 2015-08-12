9908b84a622d1bd4eb89e187210d3d4b0735b552
craigstar <craig@idlecars.com>
2015-08-12 15:57:36 -0400

    popup directive

--- src/app/drivers/bookings.controller.js
+++ src/app/drivers/bookings.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('idlecars')
 .controller('bookings.controller', function ($scope, BookingService, MyDriverService) {
-
+  var bookingId;
   var initScope = function (me) {
     $scope.username = me.client_display;
   }
@@ -16,9 +16,14 @@ angular.module('idlecars')
   MyDriverService.get().then(initScope);
   _getBooking();
 
-  $scope.cancelBooking = function (bookingId) {
+  $scope.cancelBooking = function () {
+    $scope.showPopup = false;
     var patchData = { state: '12' };
 
     BookingService.patch(bookingId, patchData).then(_getBooking);
   }
+
+  $scope.dismiss = function () { $scope.showPopup = false; }
+  $scope.popup = function (id) { $scope.showPopup = true; bookingId = id }
+
 })
---
+++ src/components/pop_up/directive.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('idlecars')
+.directive('popUp', function () {
+  return {
+    templateUrl: 'components/pop_up/pop_up.html',
+  }
+})
