d801216cb09a23e89db3c91d39d458f30c2f79af
craigstar <craig@idlecars.com>
2015-12-08 15:00:37 -0500

    delete owner stuff

--- src/app/renewals/service.js
+++
@@ -1,9 +0,0 @@
-angular.module('idlecars')
-.factory('RewnewalService', function($resource, config) {
-  return $resource(config.api_base_url + 'renewals/:renewalId/', null, {
-    renew: {
-      method: 'PATCH',
-      params: {renewalId: "@renewalId"}
-    }
-  });
-});
