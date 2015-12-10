c347152f0373d89197ce4e9fb3884146aaad16ed
craigstar <craig@idlecars.com>
2015-12-10 12:06:54 -0500

    put onborading controllers in one place

--- src/app/drivers/onboarding.controller.js
+++ src/app/drivers/onboarding.controller.js
@@ -29,3 +29,46 @@ angular.module('idlecars')
   $scope.fieldName = 'address_proof_image';
   $scope.uploadTitle = 'a bill with your address on it';
 })
+
+.controller('driver.onboarding.email.controller', function ($scope, $rootScope, $timeout, $state, MyDriverService) {
+  if (!$scope.user) { $scope.user = {} }
+
+  $scope.fields = [{
+    label: 'Enter your email address',
+    placeholder: 'email@address.com',
+    name: 'email',
+    type: 'email',
+    maxlength: '254',
+    autoFocus: true,
+  }];
+
+  $rootScope.navGoNext = function() {
+    MyDriverService.patch($scope.user).then(function () {
+      $state.go('^.referral');
+    })
+  }
+
+  $scope.validateForm = function() {
+    $timeout(function () { $rootScope.navNextEnabled = $scope.fieldForm.$valid })
+  }
+})
+
+.controller('driver.onboarding.referral.controller', function ($scope, $rootScope, $timeout, MyDriverService, RequireAuthService) {
+  if (!$scope.user) { $scope.user = {} }
+
+  $scope.fields = [{
+    label: 'Enter your referral code (optional)',
+    name: 'invitor_code',
+    type: 'text',
+    maxlength: '254',
+    autoFocus: true,
+  }];
+
+  $rootScope.navGoNext = function() {
+    MyDriverService.patch($scope.user).then(RequireAuthService.resolve)
+  }
+
+  $scope.validateForm = function() {
+    $timeout(function () { $rootScope.navNextEnabled = $scope.fieldForm.$valid })
+  }
+})
