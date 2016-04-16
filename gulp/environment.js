e681033d789a52c411b6ea19ba6a221c80936c35
Jeremy Parker <github@jeremyparker.org>
2016-04-16 11:18:07 -0400

    set config info for All Taxi

--- gulp/environment.js
+++ gulp/environment.js
@@ -7,7 +7,7 @@ gulp.task('environment', function() {
   gulp.environment = {};
 
   var prod = {
-    deploy_bucket: 'app.idlecars.com',
+    deploy_bucket: 'app.alltaxi.com',
     template_vars: {
       heap_app_id: '3053705704',
       sumome_id: 'f2576e7f90d583ed4c3672be45ee563f449874ab22f67aca0f6c1a8fb1fae21f',
@@ -15,7 +15,7 @@ gulp.task('environment', function() {
   };
 
   var staging = {
-    deploy_bucket: 'app.staging.idlecars.com',
+    deploy_bucket: 'app.staging.alltaxi.com',
     template_vars: {
       heap_app_id: '1900221263',
       sumome_id: '4261b68d16d1e620c08aa600a45b0f9cf7f6bc6b667facff026c32fb490e3255',
