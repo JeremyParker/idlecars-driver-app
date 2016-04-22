563234e9dcc7f5f679e8714e7588105896fc3122
Jeremy Parker <github@jeremyparker.org>
2016-04-22 19:14:49 -0400

    text changes

--- src/app/bookings/docs_overview.controller.js
+++ src/app/bookings/docs_overview.controller.js
@@ -17,7 +17,7 @@ angular.module('idlecars')
         isUploaded: me.fhv_license_image,
       },
       {
-        title: 'MVR',
+        title: 'MVR (upload or skip)',
         sref: 'driverAccount.onboarding.uploadAddressProof',
         isUploaded: me.address_proof_image,
       },
